36837f16f060727087d94918c6c1ca52a51d5c28
unknown <shaunak12wankhade@ggmail.com>
2021-10-23 10:02:53 +0530

    [Shaunak] Added : Simple linkedlist

--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace Linkedlist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LinkedList");
+            //Console.WriteLine("Welcome to LinkedList");
+
+            {
+                LinkedList<int> list = new LinkedList<int>();
+                list.AddLast(56);
+                list.AddLast(30);
+                list.AddLast(70);
+
+                CustomLinkedList customLinkedList = new CustomLinkedList();
+                customLinkedList.InsertLast(56);
+                customLinkedList.InsertLast(30);
+                customLinkedList.InsertLast(70);
+                customLinkedList.Display();
+                Console.ReadLine();
+            }
         }
     }
 }
